068b713b4e08c4ff2344af473668854f087d6a7e
Yltsi <ylinemi@gmail.com>
2025-03-02 16:16:26 +0200

    Time counter for database requests.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 """Main application entry point for PDM Lite."""
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 import config
 import auth
 import items
+import time
 
 app = Flask(__name__)
 app.secret_key = config.secret_key
@@ -30,6 +31,19 @@ app.add_url_rule('/statistics', 'statistics', items.statistics)
 def forbidden(e):
     """Render a 403 error page."""
     return render_template("403.html"), 403
+
+# Printing elapsed time for each request
+@app.before_request
+def before_request():
+    """Set the start time for the request."""
+    g.start_time = time.time()
+
+@app.after_request
+def after_request(response):
+    """Log the elapsed time for the request."""
+    elapsed_time = round(time.time() - g.start_time, 2)
+    print("elapsed time:", elapsed_time, "s")
+    return response
 
 if __name__ == "__main__":
     app.run(debug=True)
